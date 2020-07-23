e19f504531f6681d1d61234f532a24ce48e9dd09
twchen88 <1030chloechen@gmail.com>
2020-07-22 23:02:10 -0400

    Create styles_20.js

---
+++ styles_20.js
@@ -0,0 +1,40 @@
+import {StyleSheet} from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "white"
+    },
+    modalView: {
+        flex: 0.95,
+        backgroundColor: "white",
+        borderRadius: 10,
+        top: 62.5
+    },
+    header: {
+        flex: 1, 
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'flex-end',
+        bottom: 5,
+        backgroundColor: "#B60303"
+    },
+    body: {
+        flex: 5.8,
+        backgroundColor: "white",
+        flexDirection: "column"
+    },
+    button2: { // create a new group
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#B60303",
+        padding: 10,
+        borderRadius: 10,
+        marginLeft: 20,
+        marginRight: 20,
+        marginTop: 10,
+        marginBottom: 5
+    }
+})
+
+export default styles; 
