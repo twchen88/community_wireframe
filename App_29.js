623dde802aca59d42e8765b72c82e9155860bcf5
twchen88 <1030chloechen@gmail.com>
2020-07-26 16:21:27 -0400

    Update App_29.js

--- App_29.js
+++ App_29.js
@@ -3,12 +3,15 @@ import { Image, StyleSheet, View, Text, TouchableOpacity, ImagePropTypes, Modal,
 import { SearchBar } from 'react-native-elements';
 import styles from './styles.js';
 import listIcon from './assets/list.png';
+import Profiles from './Profiles.js';
 
-const Person = (props) => {
+
+
+const AddButton = () => {
     return (
-        <View style={styles.person}>
-            
-        </View>
+        <TouchableOpacity style={styles.addbutton}>
+            <Text style={{color: "white", fontSize: 14}}> +Add </Text>
+        </TouchableOpacity>
     )
 }
 
@@ -28,6 +31,10 @@ const App = () => {
             </View>
 
             <View style={styles.body}>
+                <TouchableOpacity style={{backgroundColor: "#EFE8E8", padding: 15, flexDirection: "row", justifyContent: "space-around"}}>
+                    <Image source={require("./assets/Vector_46.png")} style={{marginTop:7, marginLeft: -15}}></Image>
+                    <Text style={{fontSize: 30, fontWeight: "600", marginLeft: -15}}> Collaboration Group</Text>
+                </TouchableOpacity>
                 <Modal
                     animationType="slide"
                     transparent={true}
@@ -48,6 +55,14 @@ const App = () => {
                         </View>
 
                         <View style={{flex: 1}}>
+                            <View
+                                style={{
+                                borderBottomColor: '#CBCBCB',
+                                borderBottomWidth: 1,
+                                marginLeft: 7.5,
+                                marginRight: 7.5,
+                                bottom: 7.5
+                                }} />
                             <SearchBar
                                     placeholder="Search"
                                     platform={Platform.OS}
@@ -58,9 +73,19 @@ const App = () => {
 
                         <View style={{flex: 10, paddingTop: 25}}> 
                             <Text style={{fontWeight: "bold", fontSize: 16, paddingLeft: 7.5}}> Select Participants </Text>
-                            <FlatList>
-
-                            </FlatList>
+                            <FlatList
+                                data={Profiles}
+                                keyExtractor={(item, index) => item.id}
+                                renderItem={({item}) => (
+                                    <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: "center", paddingBottom: 10}}>
+                                        <View style={styles.person}>
+                                            <Image source={item.src} style={styles.pfp} />
+                                            <Text style={{paddingLeft: 20}} >{item.name}</Text>
+                                        </View>
+                                        <AddButton style={{right: 20}}></AddButton>
+                                    </View>
+                                )}
+                            />
                         </View>
 
 
@@ -80,4 +105,4 @@ const App = () => {
     );
 }
 
-export default App; 
+export default App;
