2faa6a51ca88214c163d8f5d3d8e3c008c7e0058
twchen88 <1030chloechen@gmail.com>
2020-07-26 16:22:03 -0400

    Create Profiles_29.js

---
+++ Profiles_29.js
@@ -0,0 +1,38 @@
+const Profiles=[
+	{
+	  "id" : "1",
+	  "name" : "Milannie Marasigan",
+    "src" : require('./assets/milannie.png')
+  },
+	{
+	"id" : "2",
+	"name" : "Elsa Fernandez",
+  "src" : require('./assets/elsa.png')
+	},
+	{
+	"id" : "3",
+	"name" : "Alex Wilson",
+  "src" : require('./assets/alex.png')
+	},
+	{
+	"id" : "4",
+	"name" : "Amara Pedrosa",
+  "src" : require('./assets/amara.png')
+	},
+	{
+	"id" : "5",
+	"name" : "Cece Liu",
+  "src" : require('./assets/cece.png')
+	},
+	{
+	"id" : "6",
+	"name" : "Hana Smith",
+  "src" : require('./assets/hana.png')
+  },
+  {
+    "id" : "7",
+    "name" : "Antonio Thompson",
+    "src" : require('./assets/antonio.png')
+  }
+]
+export default Profiles;
