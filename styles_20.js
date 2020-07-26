9a58c8b5fd5e2efbad0aa2e99e053e6edb3c66ce
twchen88 <1030chloechen@gmail.com>
2020-07-26 17:40:06 -0400

    Update styles_20.js

--- styles_20.js
+++ styles_20.js
@@ -16,7 +16,6 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         justifyContent: 'space-between',
         alignItems: 'flex-end',
-        bottom: 5,
         backgroundColor: "#B60303"
     },
     body: {
