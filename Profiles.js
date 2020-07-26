d3d9634ddb236e04a18fb0a7cf583b2d4be23a49
twchen88 <1030chloechen@gmail.com>
2020-07-26 15:20:23 -0400

    Create Profiles.js

---
+++ Profiles.js
@@ -0,0 +1,21 @@
+const Profiles=[
+	{
+	  "id" : "1",
+	  "name" : "Nina Zanarelli",
+    "src" : require('./assets/nina.png'),
+    "admin" : "true"
+  },
+	{
+	"id" : "2",
+	"name" : "Elsa Fernandez",
+  "src" : require('./assets/elsa.png'),
+  "admin" : "false"
+	},
+	{
+	"id" : "3",
+	"name" : "Alex Wilson",
+  "src" : require('./assets/alex.png'),
+  "admin" : "false"
+	}
+]
+export default Profiles;
